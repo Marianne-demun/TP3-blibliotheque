960202df6832cc221d3496653f03b99e19e55d08
alexisbarthes0 <abarthesbrisson@gmail.com>
2025-04-18 14:31:17 +0200

    Create Abonne.php

---
+++ appstarter/app/Models/Abonne.php
@@ -0,0 +1,14 @@
+<?php
+// namespace app\Models;
+// use CodeIgniter\Model;
+// class Abonne extends Model
+// {
+//     protected $table = 'abonne';
+//     protected $primaryKey ='matricule_abonne';
+//     protected $allowFields = ['matricule_abonne', 'nom_abonne'];
+// }
+// function getAbonneByMatricule($matricule)
+// {
+//     return $this->find($matricule);
+// }
+?>
