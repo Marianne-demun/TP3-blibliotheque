81ea30435b9019732972ea731a86abcd0a3afe6c
Marianne-demun <marianne.demun@gmail.com>
2025-04-18 13:46:42 +0200

    filtreroute

--- appstarter/app/Config/Routes.php
+++ appstarter/app/Config/Routes.php
@@ -6,8 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index', ['filter' => \App\Filters\AuthenticatedFicater::class]);
-$routes->get('/home', 'Home':: \App\Filters\AuthenticatedFicater::class)
+$routes->get('/home', 'Home', ['filter' => \App\Filters\AuthenticatedFicater::class]);
 $routes->get('/login', 'Connection::index');
-$routes->post('/login', 'Connection::attemptLogin');
-$routes->get('/aboutus', 'Connection::aboutus');
-$routes->post('/aboutus', 'Connection::aboutus');
+$routes->post('/login', 'Connection::attemptLogin'); 
