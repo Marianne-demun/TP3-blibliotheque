a4bf28caecc9e2a7fe34edf4b04f07630d40fdc7
Marianne-demun <marianne.demun@gmail.com>
2025-04-25 16:00:39 +0200

    logginadminok

--- appstarter/app/Filters/Isloggedin.php
+++ appstarter/app/Filters/Isloggedin.php
@@ -6,7 +6,7 @@ use CodeIgniter\Filters\FilterInterface;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 
-class IsLoggedin implements FilterInterface
+class IsLoggedIn implements FilterInterface
 {
     public function before(RequestInterface $request, $args = null)
     {
