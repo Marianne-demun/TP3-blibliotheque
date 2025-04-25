364e35bd61135394b57aa246962d6954df93cb84
alexisbarthes0 <abarthesbrisson@gmail.com>
2025-04-25 16:20:46 +0200

    Post Aboutus
    About Us poster et disponible via le header
    Vive les routes

--- appstarter/app/Config/Routes.php
+++ appstarter/app/Config/Routes.php
@@ -9,3 +9,5 @@ $routes->get('/', 'Home::index', ['filter' => \App\Filters\IsLoggedin::class]);
 $routes->get('/home', 'Home', ['filter' => \App\Filters\IsLoggedin::class]);
 $routes->get('/login', 'Connection::index');
 $routes->post('/login', 'Connection::attemptLogin'); 
+$routes->post('/aboutus', 'Connection::aboutus');
+$routes->get('/aboutus', 'Connection::aboutus');
