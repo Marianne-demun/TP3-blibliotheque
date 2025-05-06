981c2b05c35bd4ca09c5fc84feac861de1ed4e36
Marianne-demun <marianne.demun@gmail.com>
2025-05-06 16:12:54 +0200

    barenav1

--- appstarter/app/Views/espaceabonne.php
+++ appstarter/app/Views/espaceabonne.php
@@ -8,12 +8,28 @@ require_once(APPPATH.'Views/templates/header.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/styles.css" rel="stylesheet" />
     <title>espaceabonne</title>
 </head>
 <body>
 <form method="POST" action="/espaceabonne">
 
     <p>BLABLABLA</p>
+
+    <nav class="navbare">
+        <p class="menu">Menu</p>
+        <div class="navlinks">
+            <ul>
+                <li>Gestion des abonnés</li>
+                <li>Gestion des livres</li>
+                <li>Gestion des exemplaires</li>
+                <li>Gestion des emprunts</li>
+                <li>Gestion des retours</li>
+                <li>Gestion des demandes</li>
+            </ul>
+        </div>
+    </nav>            
+
 </body>
 <?php
 // require_once("header.php");
